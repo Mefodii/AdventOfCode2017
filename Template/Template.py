03c581c7d8f8d310ff5e864b1e37bcd53d4718b5
Mefodii <mefodii04@gmail.com>
2017-12-13 13:20:39 +0200

    Day 6 - Final solution

--- Template/Template.py
+++ Template/Template.py
@@ -1,15 +1,17 @@
 
-
 # Prepare libs
 import time
 
+# Functions
+
 # Prepare input
 start = time.time()
-f = open('../Input/Day#-B.txt', 'r')
+f = open('../Input/Day#-A.txt', 'r')
 
 # Process
 print("##--RESULT--##")
 print("Some result: ")
+
 # Execution time
 end = time.time()
 print(end - start)
