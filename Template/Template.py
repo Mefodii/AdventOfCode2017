be496ea335f1071043e08dc73c7e4a58687651ca
Mefodii <mefodii04@gmail.com>
2017-12-14 10:19:56 +0200

    More generic template

--- Template/Template.py
+++ Template/Template.py
@@ -4,14 +4,25 @@ import time
 
 # Functions
 
-# Prepare input
-start = time.time()
-f = open('../Input/Day#-A.txt', 'r')
+# Main function
+def __main__(input_file):
+    print("##--RESULT--##")
+    print("Some result: ")
 
 # Process
-print("##--RESULT--##")
-print("Some result: ")
+if __name__ == "__main__":
+    # Start time of the program
+    start = time.time()
 
-# Execution time
-end = time.time()
-print(end - start)
+    # Input file
+    input_file = open('../Input/Day3-A.txt', 'r')
+
+    # Main functionality
+    __main__(input_file)
+
+    # End time of the program
+    end = time.time()
+    # Running time of the program
+    print("Program ran for: ", end - start, "seconds.")
+
+
