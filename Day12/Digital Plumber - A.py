9ded6a68c5eb183e7045f2fc09f8f2e33a9c758b
Mefodii <mefodii04@gmail.com>
2017-12-18 17:52:52 +0200

    Day 12 - Beta

---
+++ Day12/Digital Plumber - A.py
@@ -0,0 +1,139 @@
+# --- Day 12: Digital Plumber ---
+# Walking along the memory banks of the stream, you find a small village that is experiencing a little confusion: some
+# programs can't communicate with each other.
+#
+# Programs in this village communicate using a fixed system of pipes. Messages are passed between programs using these
+# pipes, but most programs aren't connected to each other directly. Instead, programs pass messages between each other
+# until the message reaches the intended recipient.
+#
+# For some reason, though, some of these messages aren't ever reaching their intended recipient, and the programs
+# suspect that some pipes are missing. They would like you to investigate.
+#
+# You walk through the village and record the ID of each program and the IDs with which it can communicate directly
+# (your puzzle input). Each program has one or more programs with which it can communicate, and these pipes are
+# bidirectional; if 8 says it can communicate with 11, then 11 will say it can communicate with 8.
+#
+# You need to figure out how many programs are in the group that contains program ID 0.
+#
+# For example, suppose you go door-to-door like a travelling salesman and record the following list:
+#
+# 0 <-> 2
+# 1 <-> 1
+# 2 <-> 0, 3, 4
+# 3 <-> 2, 4
+# 4 <-> 2, 3, 6
+# 5 <-> 6
+# 6 <-> 4, 5
+# In this example, the following programs are in the group that contains program ID 0:
+#
+#   -   Program 0 by definition.
+#   -   Program 2, directly connected to program 0.
+#   -   Program 3 via program 2.
+#   -   Program 4 via program 2.
+#   -   Program 5 via programs 6, then 4, then 2.
+#   -   Program 6 via programs 4, then 2.
+#
+# Therefore, a total of 6 programs are in this group; all but program 1, which has a pipe that connects it to itself.
+#
+# How many programs are in the group that contains program ID 0?
+#
+#######################################################################################################################
+# Prepare libs
+#######################################################################################################################
+import time
+
+
+#######################################################################################################################
+# Functions
+#######################################################################################################################
+def program_index_by_name(name, program_list):
+    for i in range(0, len(program_list)):
+        if program_list[i]['Name'] == name:
+            return i
+
+    return -1
+
+
+def programs_reset(programs):
+    for program in programs:
+        program["Visited"] = False
+
+    return programs
+
+
+def add_program(raw_input):
+    processed_input = process_input(raw_input)
+    program_name = processed_input[0]
+    children = processed_input[1]
+    visited_flag = False
+
+    return {"Name": program_name, "Children": children, "Visited": visited_flag}
+
+
+def process_input(raw_input):
+    split = raw_input.split(" <-> ")
+    node_name = split[0]
+    children = split[1].split(", ")
+
+    return [node_name, children]
+
+
+def calculate_connections_to_program(destination_program_name, programs):
+    connections = 0
+
+    for program in programs:
+        programs_copy = programs_reset(programs)
+        index = program_index_by_name(program.get("Name"), programs)
+        current_program = programs_copy[index]
+        if check_connection_to_program(current_program, destination_program_name, programs_copy):
+            connections += 1
+
+    return connections
+
+
+def check_connection_to_program(current_program, destination_program_name, programs):
+    current_program["Visited"] = True
+
+    if current_program.get("Name") == destination_program_name:
+        return True
+    else:
+        for child in current_program.get("Children"):
+            program_child = programs[program_index_by_name(child, programs)]
+            if not program_child.get("Visited"):
+                if check_connection_to_program(program_child, destination_program_name, programs):
+                    return True
+
+    return False
+
+
+#######################################################################################################################
+# Main function
+#######################################################################################################################
+def __main__(input_file):
+    programs = []
+    for input_line in input_file:
+        programs.append(add_program(input_line.replace("\n", "")))
+
+    programs_connected = calculate_connections_to_program("0", programs)
+
+    print("##--RESULT--##")
+    print("Programs connected to 0:", programs_connected)
+
+
+#######################################################################################################################
+# Process
+#######################################################################################################################
+if __name__ == "__main__":
+    # Start time of the program
+    start = time.time()
+
+    # Input file
+    input_file = open('../Input/Day12-A.txt', 'r')
+
+    # Main functionality
+    __main__(input_file)
+
+    # End time of the program
+    end = time.time()
+    # Running time of the program
+    print("Program ran for: ", end - start, "seconds.")
